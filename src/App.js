dddf70daa215d08a09fda79230646ac0f46dfc7c
Ivan Abadzhiev <abadzhievltd@gmail.com>
2019-05-29 10:03:30 +0300

    Added redux to the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// Redux dependencies
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/reducers";
+
 //Pages
-import Home from "./scenes/Home";
+import Home from "./scenes/Home/view/Home";
 import Article from "./scenes/Article";
 import NotFound from "./scenes/NotFound/view/NotFound";
+
+const middlewares = [thunk];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/article/:id" component={Article} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/article/:id" component={Article} />
+              <Route path="*" component={NotFound} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
