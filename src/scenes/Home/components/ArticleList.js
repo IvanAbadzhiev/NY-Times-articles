7d3e4c13d7ea54884e9ab16db6d31496a2eccaa0
Ivan Abadzhiev <abadzhievltd@gmail.com>
2019-05-29 10:58:17 +0300

    Added ArticleList Component

---
+++ src/scenes/Home/components/ArticleList.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class ArticleList extends Component {
+  goToArticle = articleId => {
+    // Open the article page
+    this.props.history.push(`/article/${articleId}`);
+  };
+
+  render() {
+    return this.props.articles.map((article, index) => {
+      // TODO: create component for article
+      return (
+        <div
+          key={article.id}
+          onClick={() => {
+            this.goToArticle(index);
+          }}
+        >
+          <img
+            src={article.media[0]["media-metadata"][0].url}
+            alt={article.media[0].caption}
+          />
+          <h1>{article.title}</h1>
+          <small>{article.published_date}</small>
+          <p>{article.abstract}</p>
+        </div>
+      );
+    });
+  }
+}
+
+export default ArticleList;
